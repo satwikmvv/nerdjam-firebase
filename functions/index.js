44df10beb4ee337cff44d32f5926845537ab3a79
satwikmvv <satwikmvv@gmail.com>
2019-07-07 00:57:18 -0500

    Token Authorization

--- functions/index.js
+++ functions/index.js
@@ -47,11 +47,39 @@ app.get('/yaps', (req,res) => {
 })
 
 
-app.post('/yaps',(req,res) => {
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    }
+    else {
+        console.error('no token found')
+        return res.status(403).json({ error: 'Unauthorized'});
+    }
 
+    admin.auth().verifyIdToken(idToken)
+    .then(decodedToken => {
+        req.user = decodedToken;
+        console.log(decodedToken)
+        return db.collection('users')
+            .where('userId', '==', req.user.uid)
+            .limit(1)
+            .get()
+    })
+    .then(data => {
+        req.user.handle = data.docs[0].data().handle;
+        return next();
+    })
+    .catch(err => {
+        console.error('Error verifying token', err)
+        return res.status(403).json(err);
+    })
+}
+
+app.post('/yaps',FBAuth, (req,res) => {
     const newYap = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
@@ -61,8 +89,8 @@ app.post('/yaps',(req,res) => {
             res.json({ message: `document ${doc.id} created!`});
         })
         .catch(err => {
-            return res.status(500).json({ error: `Shit hit the fan! ${err}`});
             console.error(error);
+            return res.status(500).json({ error: `Shit hit the fan! ${err}`});
         })
 })
 
