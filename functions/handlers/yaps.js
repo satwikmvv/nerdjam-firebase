ec5dc81aa9a66d955e33b8e4cfa3ddc7d7c3e4d5
satwikmvv <satwikmvv@gmail.com>
2019-07-07 22:40:19 -0500

    like,unlike and delete

--- functions/handlers/yaps.js
+++ functions/handlers/yaps.js
@@ -24,13 +24,18 @@ exports.postOneYap = (req,res) => {
     const newYap = {
         body: req.body.body,
         userHandle: req.user.handle,
-        createdAt: new Date().toISOString()
+        userImage: req.user.imageUrl,
+        createdAt: new Date().toISOString(),
+        likeCount:0,
+        commentCount:0
     };
 
     db.collection('yaps')
         .add(newYap)
         .then(doc => {
-            res.json({ message: `document ${doc.id} created!`});
+            const resYap = newYap;
+            resYap.yapId = doc.id;
+            res.json(resYap);
         })
         .catch(err => {
             console.error(err);
@@ -58,10 +63,33 @@ exports.getYap = (req,res) => {
         return res.json(yapData)
     })
     .catch(err => {
-        console.error(err);
+        console.log(err);
         return res.status(500).json({ error: err.code});
     })
 
+}
+
+exports.deleteYap = (req,res) => {
+    const document = db.doc(`/yaps/${req.params.yapId}`);
+    document.get()
+    .then(doc => {
+        if(!doc.exists) {
+            return res.status(404).json({error:'yap not found'});
+        }
+        if(doc.data().userHandle !==req.user.handle){
+            return res.status(403).json({error:'Unauthorized'});
+        }
+        else{
+            return document.delete();
+        }
+    })
+    .then(()=>{
+        res.json({message: 'Yap Deleted'})
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({ error: err.code});
+    })
 }
 
 exports.commentOnYap = (req,res) => {
@@ -78,8 +106,10 @@ exports.commentOnYap = (req,res) => {
     db.doc(`/yaps/${req.params.yapId}`).get()
     .then(doc => {
         if(!doc.exists) return res.status(404).json({error:'yap not found'})
+        return doc.ref.update({ commentCount: doc.data().commentCount + 1 })
+    })
+    .then(()=>{
         return db.collection('comments').add(newComment);
-
     })
     .then(() => {
         res.json(newComment);
@@ -89,3 +119,88 @@ exports.commentOnYap = (req,res) => {
         return res.status(500).json({ error: err.code});
     })
 }
+
+exports.likeYap = (req, res) => {
+    const likeDocument = db.collection('likes').where('userHandle', "==", req.user.handle)
+        .where('yapId', '==', req.params.yapId).limit(1);
+    
+    const yapDocument = db.doc(`/yaps/${req.params.yapId}`);
+
+    let yapData;
+
+    yapDocument.get()
+        .then(doc => {
+            if(doc.exists){
+                yapData = doc.data();
+                yapData.yapId = doc.id;
+                return likeDocument.get();
+            }
+            else {
+                return res.status(404).json({empty:'yap not found'})
+            }
+        })
+        .then(data => {
+            if(data.empty){
+                return db.collection('likes').add({
+                    yapId: req.params.yapId,
+                    userHandle: req.user.handle
+                })
+                .then(() => {
+                    yapData.likeCount++
+                    return yapDocument.update({ likeCount: yapData.likeCount })
+                })
+                .then(()=> {
+                    return res.json(yapData);
+                })
+            }
+            else {
+                return res.status(400).json({error:'yap already liked'})
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({error:err.code})
+        })
+}
+
+exports.unlikeYap =(req,res)=>{
+    const likeDocument = db.collection('likes').where('userHandle', "==", req.user.handle)
+        .where('yapId', '==', req.params.yapId).limit(1);
+    
+    const yapDocument = db.doc(`/yaps/${req.params.yapId}`);
+
+    let yapData;
+
+    yapDocument.get()
+        .then(doc => {
+            if(doc.exists){
+                yapData = doc.data();
+                yapData.yapId = doc.id;
+                return likeDocument.get();
+            }
+            else {
+                return res.status(404).json({empty:'yap not found'})
+            }
+        })
+        .then(data => {
+            if(data.empty){
+                return res.status(400).json({error:'yap not liked'})
+                
+            }
+            else {
+                return db.doc(`/likes/${data.docs[0].id}`).delete()
+                .then(()=>{
+                    yapData.likeCount--;
+                    return yapDocument.update({ likeCount: yapData.likeCount });
+                })
+                .then(()=> {
+                    res.json(yapData);
+                })
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({error:err.code})
+        })
+}
+
--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 app.use(cors())
 
 
-const { getAllYaps, postOneYap, getYap, commentOnYap } = require('./handlers/yaps');
+const { getAllYaps, postOneYap, getYap, deleteYap, commentOnYap, likeYap, unlikeYap } = require('./handlers/yaps');
 
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
@@ -14,7 +14,10 @@ const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = req
 app.get('/yaps', getAllYaps)
 app.post('/yap',FBAuth, postOneYap)
 app.get('/yap/:yapId', getYap)
+app.delete('/yap/:yapId', FBAuth, deleteYap)
 app.post('/yap/:yapId/comment', FBAuth, commentOnYap)
+app.get('/yap/:yapId/like', FBAuth, likeYap)
+app.get('/yap/:yapId/unlike', FBAuth, unlikeYap)
 
 //signup and login routes
 
